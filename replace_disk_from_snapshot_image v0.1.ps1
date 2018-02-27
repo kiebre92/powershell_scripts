a907f1c41459bae69a3442c792dba1ef9f159991
Unknown <kieran.d.brewer@gmail.com>
2018-02-27 12:16:54 +0000

    Test from ISE not VScode

--- replace_disk_from_snapshot_image v0.1.ps1
+++ replace_disk_from_snapshot_image v0.1.ps1
@@ -7,7 +7,12 @@
 - Create new disk from snapshot (an already existing one) - Work out how to select the correct snapshot (naming convention??)
 - Mount disk
 - Save VM config
-- Turn on VM#
+- Turn on VM
+
+
+Testing ISE Update on file
 #>
+
+
 
 
