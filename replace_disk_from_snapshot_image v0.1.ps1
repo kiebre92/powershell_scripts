678743f1683a002e5d7d0f4cc4ae47237856b362
Unknown <kieran.d.brewer@gmail.com>
2018-02-27 17:25:59 +0000

    Start of script

--- replace_disk_from_snapshot_image v0.1.ps1
+++ replace_disk_from_snapshot_image v0.1.ps1
@@ -1,7 +1,7 @@
 ﻿<# 
 
 - Save current disk info to variable
-- Turn off VM
+- Turn off VM 
 - Unmount current disk from VM
 - Save config (disk unattached)
 - Create new disk from snapshot (an already existing one) - Work out how to select the correct snapshot (naming convention??)
@@ -13,6 +13,26 @@
 Testing ISE Update on file
 #>
 
+##  Variables
+$VMname = "SQL-REPLAY2"
+$VMrg = "KB-AF"
+$VMlocation = "North Europe"
 
 
+#  Stop the VM, required when working with disks
+Stop-AzureRmVM -Name $VMname -ResourceGroupName $VMrg
 
+#  Save VM info to a variable
+$VMdetails = Get-AzureRmVM -ResourceGroupName $VMrg -Name $VMname
+
+#  Removes first data disk (LUN 0)
+Remove-AzureRmVMDataDisk -VM $VMdetails -DataDiskNames $VMdetails.StorageProfile.DataDisks[0].Name
+
+#  Update the VM state
+Update-AzureRmVM -ResourceGroupName $VMrg -VM $VMdetails
+
+#  Save snapshot of the correct disk - Matching by a hard string but you could use $VMname variable instead (depending on the naming convention used)
+$snapshot = Get-AzureRmSnapshot -ResourceGroupName $VMrg | where {($_.name -match "SQL" )}
+
+
+$NEWdiskconfig = New-AzureRmDiskConfig 
